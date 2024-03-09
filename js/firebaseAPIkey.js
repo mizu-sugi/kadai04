e3516be37eee40f96642fb32c829c50e06e61720
Mizu-sugi <dnnuggets_15@yahoo.co.jp>
2024-03-09 11:14:52 +0900

    キャッシュ削除

--- js/firebaseAPIkey.js
+++
@@ -1,8 +0,0 @@
-export default  {
-    apiKey: "AIzaSyDvvf167Y0kyQRUHTTrr6dxxRlkxSCFilw",
-    authDomain: "chatapp-9f0c7.firebaseapp.com",
-    projectId: "chatapp-9f0c7",
-    storageBucket: "chatapp-9f0c7.appspot.com",
-    messagingSenderId: "67384416353",
-    appId: "1:67384416353:web:b381b36e0c9a5c345ab220"
-};
